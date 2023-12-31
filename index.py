6b1d8f2a2379ba03b0b8cd1033b76a2bb0d8c139
Stuart Allan Rutherford <stalruth@gmail.com>
2024-01-01 00:14:26 +1100

    - Script is now to be run as soon as we have a players.json file - Consumes previous output - Automatically pulls and uploads teamlists

--- index.py
+++ index.py
@@ -12,6 +12,9 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('url')
 parser.add_argument('players')
+parser.add_argument('matched_output')
+parser.add_argument('unmatched_output')
+parser.add_argument('tour_name')
 
 args = parser.parse_args()
 
@@ -29,13 +32,30 @@ def get_players(url):
     for row in soup.tbody.find_all('tr'):
         if row.contents[9].string.strip() != "Masters":
             continue
+        link = row.find('a')
         results.append({
+            'truncId': row.contents[1].string.strip(),
+            'firstName': row.contents[3].string.strip(),
+            'lastName': row.contents[5].string.strip(),
             'name': f"{row.contents[3].string.strip()} {row.contents[5].string.strip()}",
             'country': row.contents[7].string.strip(),
-            'teamlist': f"https://rk9.gg{row.find('a')['href']}",
+            'trainerName': row.contents[13].string.strip(),
+            'teamlist': f"https://rk9.gg{link['href']}" if link is not None else "",
         })
 
     return results
+
+
+def compare_records(a, b):
+    if a['truncId'] != b['truncId']:
+        return False
+    if a['name'] != b['name']:
+        return False
+    if a['country'] != b['country']:
+        return False
+    if a['trainerName'] != b['trainerName']:
+        return False
+    return True
 
 
 def get_field(node, label):
@@ -83,51 +103,107 @@ def get_team(url):
     return teamlist
 
 
-with open(args.players, 'r') as infile:
-    players = json.load(infile)
+def mon_to_paste(mon):
+    name_part = mon['species'] if mon.get('name', None) is None else f"{mon['name']} ({mon['species']})"
+    first_line = name_part if mon.get('item', None) is None else f"{name_part} @ {mon['item']}"
+    moves = '\r\n'.join([f"- {move}" for move in mon['moves']])
 
-raw_players = get_players(f"https://rk9.gg/roster/{args.url}")
-input(f"{len(raw_players)} players. ENTER to continue, Ctrl-C to exit.")
+    return f"{first_line}\r\nAbility: {mon['ability']}\r\nTera Type: {mon['teraType']}\r\n{moves}"
 
-roster_players = []
-for player in raw_players:
-    print(f"Grabbing {player['name']}'s team...")
-    roster_players.append({
-        'name': player['name'],
-        'country': player['country'],
-        'team': get_team(player['teamlist'])
-    })
 
-players_out = {}
-players_left = []
+def team_to_paste(team):
+    return "\r\n\r\n".join([mon_to_paste(mon) for mon in team])
 
-for i in players:
-    candidates = [p for p in roster_players if players[i]['name'] == f"{p['name']} [{p['country']}]"]
-    if len(candidates) == 1:
-        players_out[i] = {
-            'name': players[i]['name'],
-            'team': candidates[0]['team']
+
+def remove_country(name):
+    return name.split('[')[0]
+
+
+def make_pokepaste(player, tour_name):
+    paste_name = f"{remove_country(player['name'])}'s {tour_name} Open Team List"
+    author = "VGCPastes"
+    paste_description = "Format: gen9vgc2024regulationf"
+    paste = team_to_paste(player['fullTeam'])
+    payload = {
+        'title': paste_name,
+        'paste': paste,
+        'author': author,
+        'notes': paste_description
+    }
+    response = requests.post('https://pokepast.es/create', data=payload)
+    return response.url
+
+
+if __name__ == '__main__':
+    with open(args.players, 'r') as infile:
+        players = json.load(infile)
+
+    roster_players = get_players(f"https://rk9.gg/roster/{args.url}")
+
+    players_out = {}
+    try:
+        with open(args.matched_output, 'r') as infile:
+            players_out = json.load(infile)
+    except FileNotFoundError:
+        pass
+
+    # remove loaded players from roster and pairings
+    for i in players_out:
+        current_player = players_out[i]
+        record = [p for p in roster_players if compare_records(current_player['rosterInfo'], p)][0]
+        roster_players.remove(record)
+        current_player['rosterInfo'] = record
+
+    players_left = [i for i in players if i not in players_out]
+
+    # match players from pairings and roster
+    # list comprehension so we can update the array while iterating
+    for i in [player for player in players_left]:
+        candidates = [p for p in roster_players
+                      if players[i]['name'].lower() == f"{p['name']} [{p['country']}]".lower()]
+        if len(candidates) == 1:
+            players_out[i] = {
+                'name': players[i]['name'],
+                'rosterInfo': candidates[0]
+            }
+            roster_players.remove(candidates[0])
+            players_left.remove(i)
+
+    # some players lose their country code
+    for i in [player for player in players_left]:
+        candidates = [p for p in roster_players if players[i]['name'].lower() == p['name'].lower()]
+        if len(candidates) == 1:
+            players_out[i] = {
+                'name': players[i]['name'],
+                'rosterInfo': candidates[0]
+            }
+            roster_players.remove(candidates[0])
+            players_left.remove(i)
+
+    # see if the remining players are a gimme
+    if len(players_left) == 1:
+        player_id = players_left[0]
+        players_out[player_id] = {
+            'name': players[player_id]['name'],
+            'rosterInfo': roster_players[0]
         }
-        roster_players.remove(candidates[0])
-    else:
-        players_left.append(i)
+        roster_players = []
+        players_left = []
 
-# some players lose their country code
-# list comprehension so we can update the array while iterating
-for i in [player for player in players_left]:
-    candidates = [p for p in roster_players if players[i]['name'] == p['name']]
-    if len(candidates) == 1:
-        players_out[i] = {
-            'name': players[i]['name'],
-            'team': candidates[0]['team']
-        }
-        roster_players.remove(candidates[0])
-        players_left.remove(i)
+    # upload pastes
+    for player_id in players_out:
+        player = players_out[player_id]
+        if player['rosterInfo']['teamlist'] != "" and player.get('paste') is None:
+            player_team = get_team(player['rosterInfo']['teamlist'])
+            player['team'] = [pokemon['species'] for pokemon in player_team]
+            player['fullTeam'] = player_team
+            player['paste'] = make_pokepaste(player, args.tour_name)
 
-with open('output/teams.json', 'w') as outfile:
-    json.dump(players_out, outfile, indent=2, ensure_ascii=False)
+    with open(args.matched_output, 'w') as outfile:
+        json.dump(players_out, outfile, indent=2, ensure_ascii=False)
 
-with open('output/unmatched.json', 'w') as outfile:
-    json.dump({
-        'roster': roster_players
-    }, outfile, indent=2, ensure_ascii=False)
+    with open(args.unmatched_output, 'w') as outfile:
+        json.dump({
+            'roster': roster_players,
+            'pairings': {i: {'pairingsName': players[i]['name']} for i in players_left}
+        }, outfile, indent=2, ensure_ascii=False)
